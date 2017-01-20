fc7c88fb91e5f3cb9259406ac0d5278adde91e04
Martin Konrad <konrad@frib.msu.edu>
2017-01-20 14:30:04 -0500

    Customize debian directory

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rigol-dsa815):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rigol-dsa815" command="/usr/bin/rigol-dsa815"
--- debian/rigol-dsa815.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rigol-dsa815 package
-#
-0 4	* * *	root	[ -x /usr/bin/rigol-dsa815_maintenance ] && /usr/bin/rigol-dsa815_maintenance
