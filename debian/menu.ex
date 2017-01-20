b89e320c6b8f7b9b201ef7488860d16fc3865f02
Martin Konrad <konrad@frib.msu.edu>
2017-01-20 14:22:57 -0500

    Create debian directory with debhelper
        dh_make --native --email konrad@frib.msu.edu --single --copyright mit
        -p rigol-dsa815_0.1.0

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(rigol-dsa815):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="rigol-dsa815" command="/usr/bin/rigol-dsa815"
---
+++ debian/rigol-dsa815.default.ex
@@ -0,0 +1,10 @@
+# Defaults for rigol-dsa815 initscript
+# sourced by /etc/init.d/rigol-dsa815
+# installed at /etc/default/rigol-dsa815 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
